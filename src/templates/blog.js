29c1bba5bbcd2a1b1e5d679c171755dd017fdfa9
amber-tamara <ambertjansen@gmail.com>
2020-04-27 11:19:58 +0100

    graphql query added

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,5 +1,17 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
+
+const query = graphgl`query (
+  $slug: String:
+) { 
+  markdownRemark (fields: 
+    {slug:
+      {eq: 
+        "react"
+      } } )  
+  {frontmatter {title}
+  } }`
 
 const Blog = () => {
   return <Layout>This is the blog template</Layout>
